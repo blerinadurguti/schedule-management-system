3bceb3465583473132e9367077691469afa97d1d
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2022-03-27 22:25:54 +0200

    Update DB_KNK_project.sql
    Database updated

--- db-KNK/DB_KNK_project.sql
+++ db-KNK/DB_KNK_project.sql
@@ -1,86 +1,80 @@
-create database orari;
-use orari;
+create database SistemiPerMenaxhimTeOrareve;
+use SistemiPerMenaxhimTeOrareve;
 
-create table grupet(
-gid integer not null auto_increment,
-emri varchar(50),
-viti integer,
-primary key(gid)
+create table Grupet(
+Id integer not null auto_increment,
+Emri varchar(50),
+Viti integer,
+primary key(Id)
 );
 
-create table lendet(
-lid integer not null auto_increment,
-lname varchar(50),
-primary key(lid)
+create table Lendet(
+Id integer not null auto_increment,
+Emri varchar(50),
+primary key(Id)
 );
 
-create table profesori(
-pid integer not null auto_increment,
-panme varchar(50),
-psname varchar(50),
-primary key(pid)
+create table StafiAkademik(
+Id integer not null auto_increment,
+Emri varchar(50),
+Mbiemri varchar(50),
+Pozita varchar(50),
+primary key(Id)
 );
 
-create table asistent(
-aid integer not null auto_increment,
-aname varchar(50),
-asname varchar(50),
-primary key(aid)
+create table Salla(
+Id integer not null auto_increment,
+Emri varchar(50),
+primary key(Id)
 );
 
-create table salla(
-sid integer not null auto_increment,
-sname varchar(50),
-primary key(sid)
+create table AssignedSubjectTeacher(
+Id integer not null auto_increment,
+LendetId integer,
+StafiAkademikIdProfesori integer,
+StafiAkademikIdAsistenti integer,
+SallaId integer,
+primary key(Id),
+foreign key(LendetId) references Lendet(Id),
+foreign key(StafiAkademikIdProfesori) references StafiAkademik(Id),
+foreign key(StafiAkademikIdAsistenti) references StafiAkademik(Id),
+foreign key(SallaId) references Salla(Id)
 );
 
-create table assigned_subject_teacher(
-a_s_id integer not null auto_increment,
-lid integer,
-pid integer,
-aid integer,
-sid integer,
-primary key(a_s_id),
-foreign key(lid) references lendet(lid),
-foreign key(pid) references profesori(pid),
-foreign key(aid) references asistent(aid),
-foreign key(sid) references salla(sid)
+create table Drejtimi(
+Id integer not null auto_increment,
+Emri varchar(50),
+primary key(Id)
 );
 
-create table drejtimi(
-did integer not null auto_increment,
-dname varchar(50),
-primary key(did)
+create table VitiAkademik(
+Id integer not null auto_increment,
+Viti integer,
+primary key(Id)
 );
 
-create table viti_akademik(
-vid integer not null auto_increment,
-viti integer,
-primary key(vid)
+create table AssignedSubjectDrejtimi(
+Id integer not null auto_increment,
+DrejtimiId integer,
+VitiAkademikId integer,
+primary key(Id),
+foreign key(DrejtimiId) references Drejtimi(Id),
+foreign key(VitiAkademikId) references VitiAkademik(Id)
 );
 
-create table assigned_subject_drejtimi(
-a_s_d_id integer not null auto_increment,
-did integer,
-vid integer,
-primary key(a_s_d_id),
-foreign key(did) references drejtimi(did),
-foreign key(vid) references viti_akademik(vid)
+create table Oraret(
+Id integer not null auto_increment,
+Orari varchar(30),
+primary key(Id)
 );
 
-create table oraret(
-oid integer not null auto_increment,
-orari varchar(30),
-primary key(oid)
-);
-
-create table assigned(
-assigned_id integer not null auto_increment,
-gid integer,
-lid integer,
-oid integer,
-primary key(assigned_id),
-foreign key(gid) references grupet(gid),
-foreign key(lid) references lendet(lid),
-foreign key(oid) references oraret(oid)
+create table Assigned(
+Id integer not null auto_increment,
+GrupetId integer,
+LendetId integer,
+OraretId integer,
+primary key(Id),
+foreign key(Id) references Grupet(Id),
+foreign key(Id) references Lendet(Id),
+foreign key(Id) references Oraret(Id)
 );
