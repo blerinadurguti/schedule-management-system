6271242654a0b8f5c73fd7df3a5f58bfff4f3691
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2022-05-09 13:38:31 +0200

    Update DB_KNK_project.sql

--- db-KNK/DB_KNK_project.sql
+++ db-KNK/DB_KNK_project.sql
@@ -1,5 +1,23 @@
-create database SistemiPerMenaxhimTeOrareve;
-use SistemiPerMenaxhimTeOrareve;
+create database SMO;
+use SMO;
+drop database SMO;
+-- SMO -> Sistemi per Menaxhimin e Orareve
+
+create table User(
+Id integer not null,
+Username varchar(100),
+SaltedHash varchar(256),
+Salted varchar(100),
+primary key(Id),
+foreign key(Id)references Studenti(Id)
+);
+
+insert into User(Id,Username,SaltedHash,Salted) value
+(1,"admin","1966c0c9281ce8b3d199fe7c2b1a1f49b8e461b80e3a12dda06f0d214ff2ee5f","hsZxr8V2rZOPz6rlE4yfLoGxlIvHwVf36hsXH7XdZrkqjR3rVc8oklwDb1gWIuq0");
+
+select * from User;
+select * from studenti;
+select * from grupet;
 
 create table Grupet(
 Id integer not null auto_increment,
@@ -47,11 +65,27 @@ Emri varchar(50),
 primary key(Id)
 );
 
+select * from drejtimi;
+
+insert into Drejtimi(Emri)values
+("Automatikë e Kompjuterizuar dhe Robotikë"),
+("Elektroenergjetikë"),
+("Elektronikë"),
+("Inxhinieri Kompjuterike"),
+("Telekomunikacion");
+
 create table VitiAkademik(
 Id integer not null auto_increment,
-Viti integer,
+Viti varchar(10),
 primary key(Id)
 );
+
+select * from vitiakademik;
+
+insert into vitiakademik(Viti)values
+("Viti 1"),
+("Viti 2"),
+("Viti 3");
 
 create table AssignedSubjectDrejtimi(
 Id integer not null auto_increment,
@@ -83,20 +117,17 @@ create table Studenti(
 Id integer not null auto_increment,
 Emri varchar(15),
 Mbiemri varchar(15),
-Email varchar(30),
 Viti integer,
 Grupi integer,
 Primary Key(Id),
 foreign key(Grupi) references Grupet(Id));
 
-alter table Studenti add column Drejtimi integer after Email;
+alter table Studenti add column Drejtimi integer after Mbiemri;
 alter table Studenti add foreign key(Drejtimi) references drejtimi(Id);
+alter table Studenti add column StudentId varchar(30) after Mbiemri;
 
-create table User(
-Id integer not null auto_increment,
-Username varchar(100),
-SaltedHash varchar(256),
-Salted varchar(100),
-primary key(Id),
-foreign key(Id)references Studenti(Id)
-);
+insert into studenti (Emri, Mbiemri) value
+("Admin","Admin");
+
+select * from studenti;
+select * from user;
