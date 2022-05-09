fa1f90c8027ed96e9b6ee062f9def88ae3f2c659
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2022-05-09 14:02:56 +0200

    Update

---
+++ src/Controller/HomeController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomeController {
+
+	private Stage stage;
+	private Scene scene;
+	
+    @FXML
+    void LogOut(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("/views/log_in.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+    }
+
+}
